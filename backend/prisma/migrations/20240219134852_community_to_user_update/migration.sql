f5b9cd1cabc77188886f6559cd0894ed012e79b4
MatthiasBlc <mat.bouloc@gmail.com>
2024-02-19 14:52:06 +0100

    prisma community to user update

---
+++ backend/prisma/migrations/20240219134852_community_to_user_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `CommunityToUser` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CommunityToUser" DROP COLUMN "title";
